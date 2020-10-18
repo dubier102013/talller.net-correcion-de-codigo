3871affff7f52c9db1cf1a000b11315adec94a28
dubier augusto molina roldan <dubier102013@gmail.com>
2020-10-17 20:16:44 -0500

    correcion de taller

---
+++ Olimpiadas(Vista)-PorCorregir/Controlador/posiciones/PosicionesDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Controlador.posiciones
+{
+    public class PosicionesDTO
+    {
+        private byte idposicion;
+        private string nombreposicion;
+
+        public void setIdposicion(byte valor) {
+            this.idposicion = valor;
+        }
+        public void setNombreposicion(string valor)
+        {
+            this.nombreposicion = valor;
+        }
+
+        public String getNombreposicion()
+        {
+            return this.nombreposicion;
+        }
+
+        public byte getIdposicion() {
+            return this.idposicion;
+        }
+
+    }
+}
